5971a4d727bccca09c10c4a866726318d6a10c81
AmmarFK <farooqammar51@yahoo.com>
2024-09-21 00:35:27 +1000

    workflow trigger test

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ const app = express();
 // Throws an error if the PORT environment variable is missing.
 //
 if (!process.env.PORT) {
-    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+  throw new Error(
+    "Please specify the port number for the HTTP server with the environment variable PORT."
+  );
 }
 
 //
@@ -18,21 +20,22 @@ const PORT = process.env.PORT;
 //
 // Registers a HTTP GET route for video streaming.
 //
-app.get("/video", async (req, res) => { // Route for streaming video.
-    
-    const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
-    const stats = await fs.promises.stat(videoPath);
+app.get("/video", async (req, res) => {
+  // Route for streaming video.
 
-    res.writeHead(200, {
-        "Content-Length": stats.size,
-        "Content-Type": "video/mp4",
-    });
-    fs.createReadStream(videoPath).pipe(res);
+  const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
+  const stats = await fs.promises.stat(videoPath);
+
+  res.writeHead(200, {
+    "Content-Length": stats.size,
+    "Content-Type": "video/mp4",
+  });
+  fs.createReadStream(videoPath).pipe(res);
 });
 
 //
 // Starts the HTTP server.
 //
 app.listen(PORT, () => {
-    console.log(`Microservice online.`);
+  console.log(`Microservice online. Test`);
 });
